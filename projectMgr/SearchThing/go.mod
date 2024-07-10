ebca9663cc4c98488874f14d17b06f3c7e26756a
Kacper Nitkiewicz <crg1234@gmail.com>
2024-07-10 22:17:26 +0200

    added fzf and find methods

---
+++ projectMgr/SearchThing/go.mod
@@ -0,0 +1,3 @@
+module github.com/gadzbi123/SearchThing
+
+go 1.22.0
---
+++ projectMgr/SearchThing/searchThing.go
@@ -0,0 +1,44 @@
+package SearchThing
+
+var BASEDIR string = "/mnt/g/baza_mgr/jacek"
+
+type (
+	SearchFileInterface interface {
+		FindRegularFile(string) ([]string, error)
+		FindFileByFileExtension(string) ([]string, error)
+		FindFileByRegex(string) ([]string, error)
+		FindFileInZips(string) ([]string, error)
+		FindFileWithSpecialChars() ([]string, error)
+		FindFileByPermission(string) ([]string, error)
+	}
+
+	SearchDirInterface interface {
+		FindRegularDir(string) ([]string, error)
+		FindDirByFileExtension(string) ([]string, error)
+		FindDirByRegex(string) ([]string, error)
+		FindDirInZips(string) ([]string, error)
+		FindDirWithSpecialChars(string) ([]string, error)
+	}
+
+	SearchTextInterface interface {
+		FindRegularText(string) ([]string, error)
+		FindTextByFileExtension(string) ([]string, error)
+		FindTextByRegex(string) ([]string, error)
+		FindTextInZips(string) ([]string, error)
+		FindTextWithSpecialChars(string) ([]string, error)
+	}
+
+	SearchByPiping interface {
+		todo()
+	}
+)
+
+type ProgramName int
+
+const (
+	Grep ProgramName = iota
+	Ripgrep
+	Find
+)
+
+var programExecutableList []string = []string{"grep", "ripgrep", "find","fzf"}
