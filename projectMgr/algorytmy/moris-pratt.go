ae691cc6ea767ecedafb9c62790a6d702a394606
gadzbi123 <crg1234@gmail.com>
2024-08-14 18:06:00 +0200

    update to moris-pratt

--- projectMgr/algorytmy/moris-pratt.go
+++ projectMgr/algorytmy/moris-pratt.go
@@ -22,23 +22,19 @@ func moris_pratt_slow(s []byte, substr []byte) (res []int) {
 	for i := 0; i < slen; i++ {
 		for j := 0; j < substrlen; j++ {
 			if slen <= i+j {
-				// fmt.Println("too long")
 				return
 			}
-			// fmt.Println("comparing", s[i+j], substr[j], i, j)
 			if s[i+j] != substr[j] {
 				if j > 0 {
 					pattern_part := s[i : i+j+1]
 					bb := prefix_sufix(pattern_part)
 					mov_len := len(pattern_part) - bb - 1
-					// fmt.Println("movlen", mov_len, len(pattern_part), bb)
 					i += mov_len
 				}
 				break
 			}
 			if j == substrlen-1 {
 				res = append(res, i)
-				// sufix?
 			}
 		}
 	}
@@ -61,8 +57,7 @@ func moris_pratt(s []byte, substr []byte) (res []int) {
 		preproc[i] = curr
 	}
 
-	fmt.Println("preproc", preproc)
-	// substroszukujemy substrozycji wzorca w łańcuchu
+	// szukujemy substrozycji wzorca w łańcuchu
 	curr = 0
 	found := 0
 	for i := 0; i < lenS; i++ {
